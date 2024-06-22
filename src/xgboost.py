68e5d821bf5c8d9b12a714fc0fd6fa2c950844ce
Zephyrus <zephyrus1175@yahoo.com>
2024-06-22 22:29:46 +0300

    "creating property weights for specific optimizers"

--- src/xgboost.py
+++ src/xgboost.py
@@ -7,19 +7,35 @@ Purpose: Create Model           |
 """
 import xgboost as xgb
 import optuna
-from options import get_options
+import numpy as np
 
 class XGBoost:
-    def __init__(self):
-        self.arg_params = get_options()
-        self.lr = self.arg_params.lr
-        self.optimizer = self.arg_params.optimizer
-        self.iteration = self.arg_params.iteration
-        self.gamma = self.arg_params.gamma
-        self.model = None
+    def __init__(self, args, X_train, X_test, y_train, y_test):
+        # Load data
+        self.X_train = X_train
+        self.X_test = X_test
+        self.y_train = y_train
+        self.y_test = y_test
+
+        # Load options
+        self.lr = args.lr
+        self.optimizer = args.optimizer
+        self.iteration = args.iteration
+        self.gamma = args.gamma
+        # Initialize M & N for specific optimizers
+        self.m = np.zeros_like(self.weights)
+        self.n = np.zeros_like(self.weights)
+        self.t = 0
+
+    @property
+    def _weights(self):
+        return self.weights
+    
+    @_weights.setter
+    def _weights(self, value):
+        assert isinstance(value, np.ndarray) and value.shape == self._weights.shape, "weights must be a numpy array and have the same shape as the current weights"
+        self.weights = value
         
-
-
     def fit(self, X, y):
         self.model = xgb.train(self.params, xgb.DMatrix(X, y))
 
