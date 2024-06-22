538c72a889863e65ce5540975a673626b802344c
Zephyrus <zephyrus1175@yahoo.com>
2024-06-23 01:55:10 +0300

    "Code updated. Created based DecisionTreeRegressor"

--- src/xgboost.py
+++ src/xgboost.py
@@ -5,7 +5,7 @@ Date: 06.22.2024                |
 Purpose: Create Model           |
 ---------------------------------
 """
-import xgboost as xgb
+from sklearn.tree import DecisionTreeRegressor
 import optuna
 import numpy as np
 epsilon = 1e-5
@@ -23,6 +23,10 @@ class XGBoost:
         self.optimizer = args.optimizer
         self.iteration = args.iteration
         self.gamma = args.gamma
+        self.max_depth = args.max_depth
+        self.n_estimators = args.n_estimators
+        self.initial_prediction = self.max_depth
+        self.trees = []
         # Initialize M & N for specific optimizers
         self.m = np.zeros_like(self.weights)
         self.n = np.zeros_like(self.weights)
@@ -37,15 +41,31 @@ class XGBoost:
         assert isinstance(value, np.ndarray) and value.shape == self._weights.shape, "weights must be a numpy array and have the same shape as the current weights"
         self._weights = value
     
-    def fit(self):
-        self.model = xgb.train({
-            'learning_rate': self.lr,
-            'gamma': self.gamma,
-            'objective': 'reg:squarederror'
-        }, xgb.DMatrix(self.X_train, self.y_train), num_boost_round=self.iteration)
+    def fit(self, X, y):
+        self.initial_prediction = np.mean(y)
+        prediction = np.full(y.shape, self.initial_prediction)
+
+        for _ in range(self.n_estimators):
+            gradients = y - prediction # pseudo-residuals
+
+            # Fit a regression tree to the gradients
+            tree = DecisionTreeRegressor(max_depth=self.max_depth)
+            tree.fit(X, gradients)
+
+            # Update the predictions
+            prediction += self.lr * tree.predict(X)
+
+            self.trees.append(tree)
+            
+    def predict(self, X):
+        # Start with initial prediction
+        prediction = np.full(X.shape[0], self.initial_prediction)
         
-    def predict(self):
-        return self.model.predict(xgb.DMatrix(self.X_test))
+        # Add predictions from all trees
+        for tree in self.trees:
+            prediction += self.learning_rate * tree.predict(X)
+        
+        return prediction
 
 
 
