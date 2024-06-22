4c1a52d12848bf2d790f8696468057ca62c95fa1
Zephyrus <zephyrus1175@yahoo.com>
2024-06-22 22:18:00 +0300

    "Initialiazed arg params to model class"

--- src/xgboost.py
+++ src/xgboost.py
@@ -7,11 +7,17 @@ Purpose: Create Model           |
 """
 import xgboost as xgb
 import optuna
+from options import get_options
 
 class XGBoost:
-    def __init__(self, params):
-        self.params = params
+    def __init__(self):
+        self.arg_params = get_options()
+        self.lr = self.arg_params.lr
+        self.optimizer = self.arg_params.optimizer
+        self.iteration = self.arg_params.iteration
+        self.gamma = self.arg_params.gamma
         self.model = None
+        
 
 
     def fit(self, X, y):
