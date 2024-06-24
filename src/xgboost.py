21bb2ef79391294e3e9bf4fecf4ffa4003e588b2
Zephyrus <zephyrus1175@yahoo.com>
2024-06-24 19:48:55 +0300

    "R2 metric added. And a few changes on fitting model"

--- src/xgboost.py
+++ src/xgboost.py
@@ -5,6 +5,7 @@ Date: 06.22.2024                |
 Purpose: Create Model           |
 ---------------------------------
 """
+from sklearn.metrics import r2_score
 from sklearn.tree import DecisionTreeRegressor
 import numpy as np
 epsilon = 1e-5
@@ -41,20 +42,28 @@ class XGBoost:
         self._weights = value
     
     def fit_single_iteration(self):
-        gradients = self.y_train - self.prediction # pseudo-residuals
+        # Start with initial prediction
+        prediction = np.full(self.y_train.shape, self.initial_prediction)
+        for _ in range(self.n_estimators):
+            gradients = self.y_train - prediction # pseudo-residuals
+            # Fit a regression tree to the gradients
+            tree = DecisionTreeRegressor(max_depth=self.max_depth)
+            tree.fit(self.X_train, gradients)
 
-        # Fit a regression tree to the gradients
-        tree = DecisionTreeRegressor(max_depth=self.max_depth)
-        tree.fit(self.X_train, gradients)
-
-        # Update the predictions
-        self.prediction += self.lr * tree.predict(self.X_train)
-
-        self.trees.append(tree)
+            # Update the predictions
+            self.prediction += self.lr * tree.predict(self.X_train)
+            self.trees.append(tree)
         
     def mse(self):
         return np.mean((self.y_test - self.predict()) ** 2)
-
+    
+    def r2_score(self):
+        y_mean = np.mean(self.y_test)
+        sst = np.sum((self.y_test - y_mean) ** 2)
+        ssr = np.sum((self.y_test - self.predict()) ** 2)
+        r2 = 1 - (ssr / sst)
+        return r2
+    
     def predict(self):
         # Start with initial prediction
         prediction = np.full(self.X_test.shape[0], self.initial_prediction)
