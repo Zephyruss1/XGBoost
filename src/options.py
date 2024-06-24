72be3d353c41c2c1d73a54326f1bd0c759db2cc1
Zephyrus <zephyrus1175@yahoo.com>
2024-06-24 19:49:05 +0300

    "Hyper-parameter changes"

--- src/options.py
+++ src/options.py
@@ -9,10 +9,10 @@ import argparse
 
 def get_options():
     parser = argparse.ArgumentParser(description='XGBOOST options')
-    parser.add_argument('--lr', type=float, default=3e-4, help='Learning rate for each update step')
+    parser.add_argument('--lr', type=float, default=0.01, help='Learning rate for each update step')
     parser.add_argument('--optimizer', type=str, default='SGDW', help='Using GD for update')
     parser.add_argument('--max_depth', type=int, default=3, help='Maximum depth of the tree')
-    parser.add_argument('--n_estimators', type=int, default=100, help='Maximum depth of the tree')
+    parser.add_argument('--n_estimators', type=int, default=50, help='Maximum depth of the tree')
 
     args = parser.parse_args()
     return args
