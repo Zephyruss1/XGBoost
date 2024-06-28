0aa5ad7e3e8960b5af8124ff1a693ff9e8b51168
Zephyrus <zephyrus1175@yahoo.com>
2024-06-29 00:33:05 +0300

    "bug fixed which is the causing infinity loop"

--- src/main.py
+++ src/main.py
@@ -8,11 +8,12 @@ Purpose: Run the project        |
 
 import sys
 from pathlib import Path
+
 sys.path.append(str(Path(__file__).resolve().parent.parent))
 
-import time 
+import time
 from dataset.load_dataset import LoadDataset
-from xgboost import XGBoost
+from xgbRegressor import Regressor
 from options import get_options
 import logging
 
@@ -21,20 +22,21 @@ logger = logging.getLogger(__name__)
 
 args = get_options()
 
+
 def main_run():
     if __name__ == '__main__':
         dataset_loader = LoadDataset()  # Step 1: Instantiate the class
         (X_train, X_test), (y_train, y_test) = dataset_loader.splitData()  # Step 2: Call the method on the instance
 
-        model = XGBoost(args=args, X_train=X_train, X_test=X_test, y_train=y_train, y_test=y_test)
-        print(f"[ARGS INFO] Learning Rate: {args.lr} | Max Depth: {args.max_depth} | N-Estimators: {args.n_estimators}")
-        print("---" * 10)
+        regressor = Regressor(args=args, X_train=X_train, X_test=X_test, y_train=y_train, y_test=y_test)
+        model = regressor.get_best_params()
 
         for i in range(args.n_estimators):
             t0 = time.time()
-            model.fit_single_iteration()
             t1 = time.time()
-            total_ms  = (t1 - t0) * 1000
-            print(f"[RUN INFO] Iteration: {i+1} | {total_ms:.2f} ms | MSE: {model.mse():.2f} | R2 Score: {model.r2_score():.2f}")
+            total_ms = (t1 - t0) * 1000
+            print(
+                f"[RUN INFO] Iteration: {i + 1} | {total_ms:.2f} ms | MSE: {regressor.mse(model=model):.2f} | R2 Score: {regressor.r2_score(model=model):.2f}")
+
 
 main_run()
